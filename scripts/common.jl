8880ef022fa82ca653f5144cae33ceabbfe05270
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-09-21 17:37:07 +0900

    Implement common.jl

---
+++ scripts/common.jl
@@ -0,0 +1,12 @@
+const PLUTO_NOTEBOOKS_DIR = joinpath(dirname(@__DIR__), "pluto_notebooks")
+
+const PLUTO_FILE_NAMES = [
+    "welcome.jl",
+    "quantics1d.jl",
+    "quantics1d_advanced.jl",
+    "compress.jl",
+    "interfacingwithitensors.jl",
+    "quantics2d.jl",
+    "plots.jl",
+    "qft.jl",
+]
